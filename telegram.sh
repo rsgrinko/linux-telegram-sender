18eb6c3a93565683e96723a9d20659ee002bdd78
Roman Sergeevitch Grinko <rsgrinko@gmail.com>
2024-03-13 22:44:46 +0300

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -8,6 +8,7 @@
 # Для удобства можно закинуть его или симлинк на него в /bin/telegram
 # cp telegram.sh /bin/telegram
 # Для проверки используйте команду echo test | telegram
+# Или передайте строку аргументом telegram "Hello World!"
 # Если все настроено правильно - вам придет сообщение
 #
 # Author: rsgrinko <rsgrinko@gmail.com>
@@ -15,9 +16,14 @@
 # Github: https://github.com/rsgrinko\
 
 
-API_TOKEN="insert_here_your_bot_token_please"
-CHAT_ID="insert_here_your_chat_id_please"
-MESSAGE=`cat /dev/stdin`
+API_TOKEN="5357759725:AAEPGfLRaye1ZOPMBGOrBVCMhz4kE_aecME"
+CHAT_ID="412790359"
+
+MESSAGE=$1
+if [ -z "$MESSAGE" ]
+then
+    MESSAGE=`cat /dev/stdin`
+fi
 
 TG_MESSAGE="<b><u>Message from $(hostname)</u></b>
 <b>Date: $(date  "+%d.%m.%Y %H:%M:%S")</b>

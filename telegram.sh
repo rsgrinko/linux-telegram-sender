8b2b86f4d5afdc8e56f6f3dcb3a66c63940a1f74
Roman Sergeevitch Grinko <rsgrinko@gmail.com>
2024-03-13 22:18:15 +0300

    Create telegram.sh
    add telegram.sh

---
+++ telegram.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Данный скрипт пренданзачен для отправки stdin потока в Telegram
+#
+# Для его настройки замените API_TOKEN на токен бота
+# Также измените CHAT_ID на идентификатор пользователя (чата), куда будут отправляться сообщения
+# Дайте скрипту права на выполнение chmod +x telegram.sh
+# Для удобства можно закинуть его или симлинк на него в /bin/telegram
+# cp telegram.sh /bin/telegram
+# Для проверки используйте команду echo test | telegram
+# Если все настроено правильно - вам придет сообщение
+#
+# Author: rsgrinko <rsgrinko@gmail.com>
+# Site: is-stories.ru
+# Github: https://github.com/rsgrinko\
+
+
+API_TOKEN="insert_here_your_bot_token_please"
+CHAT_ID="insert_here_your_chat_id_please"
+MESSAGE=`cat /dev/stdin`
+
+TG_MESSAGE="<b><u>Message from $(hostname)</u></b>
+<b>Date: $(date  "+%d.%m.%Y %H:%M:%S")</b>
+$MESSAGE
+"
+
+curl -s -X POST https://api.telegram.org/bot$API_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$TG_MESSAGE" -d parse_mode=html >> /dev/null
